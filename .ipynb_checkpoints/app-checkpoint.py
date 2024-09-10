5bf52d38f1f5313ebbc969c87b4f1925f5813eda
D3nz1ll <denzilng@gmail.com>
2024-09-10 22:05:06 +0800

    changing from ft model to the best accuracy model gpt 4

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -80,7 +80,7 @@ def load_data():
         index = load_index_from_storage(storage_context)
 
         # Load the finetuned model 
-        ft_model_name = "ft:gpt-3.5-turbo-1106:personal::9yLUndRk"
+        ft_model_name = "gpt-4"
         ft_context = ServiceContext.from_defaults(llm=OpenAI(model=ft_model_name, temperature=0.3), 
         context_window=2048, 
         
